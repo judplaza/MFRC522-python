7b58fc8fcaeb41f982b15ed1ff3c2659d357c22f
judplaza <58659463+judplaza@users.noreply.github.com>
2021-09-28 23:52:47 +0200

    Update _aux.py

--- mfrc522/_aux.py
+++ mfrc522/_aux.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python
 
 import RPi.GPIO as GPIO
-from . import SimpleMFRC522
+from mfrc522 import SimpleMFRC522
+class Rfid_rc522:
+        def scan_uid(self):
+                reader = SimpleMFRC522()
+                uid=reader.read_uid()
+                uid_hex=hex(uid).upper()
+                return uid_hex
+if __name__ == "__main__":
+        init()
+        try:
+                rf=Rfid_rc522()
+                uid = rf.scan_uid()
+                print(uid.strip("0X"))
+        finally:
+                GPIO.cleanup()
+                
 
-reader = SimpleMFRC522()
-
-try:
-        id = reader.read_uid()
-        print(id)        
-finally:
-        GPIO.cleanup()
