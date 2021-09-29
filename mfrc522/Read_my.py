31ae5eaa9ffcf90c8c2f3c80c24a2e2ff47bb2ad
judplaza <58659463+judplaza@users.noreply.github.com>
2021-09-29 23:32:33 +0200

    Update Read_my.py

--- mfrc522/Read_my.py
+++ mfrc522/Read_my.py
@@ -6,8 +6,9 @@ from mfrc522 import SimpleMFRC522
 reader = SimpleMFRC522()
 
 try:
-        id = reader.read_uid()
-        print(id)        
+        id = reader.read_id()
+        uid = reader.from_dec_to_hex8(id)
+        print("UID hex8: " , uid)
 finally:
         GPIO.cleanup()
                 
